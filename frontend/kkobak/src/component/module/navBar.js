d46554273a0bef4180e3488a21fef1c52c101110
cheecrma <cheecrma@gmail.com>
2022-10-25 11:16:40 +0900

    [Update] sid nav bar 생성 및 router 생성

--- frontend/kkobak/src/component/module/navBar.js
+++ frontend/kkobak/src/component/module/navBar.js
@@ -9,6 +9,7 @@ import Home from "../../static/home.svg";
 import Flag from "../../static/flag.svg";
 import User from "../../static/user.svg";
 import Settings from "../../static/settings.svg";
+import AppBar from "@mui/material/AppBar";
 
 export default function SelectedListItem() {
   const [selectedIndex, setSelectedIndex] = React.useState(0);
@@ -21,7 +22,8 @@ export default function SelectedListItem() {
   };
 
   return (
-    <Box
+    <AppBar
+      position="absolute"
       sx={{
         width: "100%",
         maxWidth: 80,
@@ -29,11 +31,13 @@ export default function SelectedListItem() {
         height: "100vh",
         borderRight: 1,
         borderColor: "#CDCDCD",
+        left: 0,
       }}
     >
       {/* 로고 위치 */}
-      <List component="nav" aria-label="main" sx={{ marginBottom: "100px" }}>
+      <List component="nav" aria-label="main" sx={{ flex: 1 }}>
         <ListItemButton
+          href="/main"
           sx={{ paddingLeft: "21px" }}
           selected={selectedIndex === 0}
           onClick={(event) => handleListItemClick(event, 0)}
@@ -43,17 +47,15 @@ export default function SelectedListItem() {
           </ListItemIcon>
         </ListItemButton>
       </List>
+      <Box sx={{ flex: 2 }} />
 
       {/* 화면 이동 nav 메인, 챌린지, 나의챌린지, 통계 등 */}
-      <List
-        component="nav"
-        aria-label="secondary"
-        sx={{ marginBottom: "330px" }}
-      >
+      <List component="nav" aria-label="secondary" sx={{ flex: 10 }}>
         <ListItemButton
+          href="/main"
           selected={selectedIndex === 1}
           onClick={(event) => handleListItemClick(event, 1)}
-          sx={{ paddingLeft: "21px", marginBottom: "30px" }}
+          sx={{ paddingLeft: "21px", marginBottom: "30px", color: "gray" }}
         >
           <Box>
             <ListItemIcon>
@@ -66,10 +68,12 @@ export default function SelectedListItem() {
             />
           </Box>
         </ListItemButton>
+
         <ListItemButton
+          href="/challenge"
           selected={selectedIndex === 2}
           onClick={(event) => handleListItemClick(event, 2)}
-          sx={{ paddingLeft: "21px", marginBottom: "30px" }}
+          sx={{ paddingLeft: "21px", marginBottom: "30px", color: "gray" }}
         >
           <Box>
             <ListItemIcon>
@@ -82,12 +86,14 @@ export default function SelectedListItem() {
           </Box>
         </ListItemButton>
         <ListItemButton
+          href="/myChallenge"
           selected={selectedIndex === 3}
           onClick={(event) => handleListItemClick(event, 3)}
           sx={{
             paddingLeft: "16px",
             paddingRight: "4px",
             marginBottom: "30px",
+            color: "gray",
           }}
         >
           <Box>
@@ -102,8 +108,9 @@ export default function SelectedListItem() {
         </ListItemButton>
       </List>
       {/* 세팅 위치 */}
-      <List component="nav" aria-label="last">
+      <List component="nav" aria-label="last" sx={{ flex: 1 }}>
         <ListItemButton
+          href="/setting"
           sx={{ paddingLeft: "21px" }}
           selected={selectedIndex === 4}
           onClick={(event) => handleListItemClick(event, 4)}
@@ -113,6 +120,6 @@ export default function SelectedListItem() {
           </ListItemIcon>
         </ListItemButton>
       </List>
-    </Box>
+    </AppBar>
   );
 }
