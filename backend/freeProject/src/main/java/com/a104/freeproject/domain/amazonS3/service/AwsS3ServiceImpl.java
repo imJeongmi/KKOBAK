da33687a2e7fef88a770cf8d5502333c9a1a8742
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-10-25 14:56:25 +0900

    Update AwsS3ServiceImpl.java

--- backend/freeProject/src/main/java/com/a104/freeproject/domain/amazonS3/service/AwsS3ServiceImpl.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/domain/amazonS3/service/AwsS3ServiceImpl.java
@@ -23,7 +23,9 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @PropertySources({
         @PropertySource("classpath:application.properties"),
-        @PropertySource("classpath:application-secret.properties")
+        @PropertySource("classpath:application-secret.properties"),
+        @PropertySource("classpath:application-aws.yml"),
+        @PropertySource("classpath:application-credentials.yml")
 })
 public class AwsS3ServiceImpl implements AwsS3Service {
     @Value("${cloud.aws.s3.bucket}")
