3e2d6fbb5a413fdff0bee5fd36f4970d94b3e302
cheecrma <cheecrma@gmail.com>
2022-10-25 11:17:39 +0900

    [Update] App 및 index에 nav바 적용을 위한 수정

--- frontend/kkobak/src/App.js
+++ frontend/kkobak/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
+import Nav from "./component/module/navBar";
+
+import RouterConfiguration from "./configs/router";
+
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <Box>
+        <Box
+          sx={{ width: "80px", height: "100vh", backgroundColor: "black" }}
+        ></Box>
+        <Box>
+          <Nav />
+        </Box>
+      </Box>
+      <RouterConfiguration />
+    </Box>
   );
 }
 
