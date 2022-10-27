f1ea32a30f3f1c84ea430b547349199c0e1c4912
Jeongmi Park <qkrwjdal419@gmail.com>
2022-10-27 14:27:31 +0900

    Revert "[Add] WatchToggle"
    This reverts commit 97be8dc7eb8a1deeb29695f4d881898e92aa3fba.

--- frontend/kkobak/src/App.js
+++ frontend/kkobak/src/App.js
@@ -4,33 +4,30 @@ import Nav from "./component/module/Navbar";
 import RouterConfiguration from "./configs/Router";
 
 import { Box } from "@mui/material";
-import WatchToggle from "./component/atom/WatchToggle";
 
 function App() {
   return (
-    // <Box
-    //   sx={{
-    //     overflow: "hidden",
-    //   }}
-    // >
-    //   <Box
-    //     sx={{
-    //       float: "left",
-    //     }}
-    //   >
-    //     <Box
-    //       sx={{
-    //         width: "80px",
-    //         height: "100vh",
-    //         backgroundColor: "black",
-    //       }}
-    //     ></Box>
-    //     <Nav />
-    //   </Box>
-    <Box>
+    <Box
+      sx={{
+        overflow: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          float: "left",
+        }}
+      >
+        <Box
+          sx={{
+            width: "80px",
+            height: "100vh",
+            backgroundColor: "black",
+          }}
+        ></Box>
+        <Nav />
+      </Box>
       <Box>
-        {/* <RouterConfiguration /> */}
-        <WatchToggle />
+        <RouterConfiguration />
       </Box>
     </Box>
   );
