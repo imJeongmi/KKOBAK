30cd48597a7e651eeeb81aee6f788506e7054463
cheecrma <cheecrma@gmail.com>
2022-10-28 00:27:27 +0900

    [Update] 챌린지 리스트 화면 와이어프레임

--- frontend/kkobak/src/component/module/ChallengeCardList.js
+++ frontend/kkobak/src/component/module/ChallengeCardList.js
@@ -1,28 +1,54 @@
 import React, { useState } from "react";
 import ChallengeCard from "./ChallengeCard";
 import Box from "@mui/material/Box";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Button from "component/atom/TextButton";
+import WatchToggle from "component/atom/WatchToggle";
 
 export default function ChallengeCardList() {
   const [ChallengeList, setChallengeList] = useState([]);
 
   return (
-    <Box
-      sx={{
-        width: "900px",
-        minHeight: "578px",
-        backgroundColor: "#F5F5F5",
-        verticalAlign: "middle",
-        display: "flex",
-        flexWrap: "wrap",
-      }}
-    >
-      {/* 받아온 챌린지리스트 순회하기 */}
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
-      <ChallengeCard />
+    <Box>
+      <Box
+        sx={{
+          width: "924px",
+          minHeight: "765px",
+          backgroundColor: "#F5F5F5",
+          verticalAlign: "middle",
+          display: "flex",
+          flexWrap: "wrap",
+          borderRadius: 5,
+        }}
+      >
+        {/* 받아온 챌린지리스트 순회하기 */}
+        <Box sx={{ marginTop: "60px" }}>
+          <ChallengeCard />
+        </Box>
+        <Box sx={{ marginTop: "60px" }}>
+          <ChallengeCard />
+        </Box>
+        <Box sx={{ marginTop: "60px" }}>
+          <ChallengeCard />
+        </Box>
+        <Box sx={{ marginBottom: "100px" }}>
+          <ChallengeCard />
+        </Box>
+        <ChallengeCard />
+        <ChallengeCard />
+      </Box>
+      <Box sx={{ marginLeft: "275px", marginTop: "-55px" }}>
+        <Stack spacing={2}>
+          <Pagination count={10} shape="rounded" />
+        </Stack>
+      </Box>
+      <Box sx={{ marginLeft: "735px", marginTop: "-100px" }}>
+        <Button size="ss">챌린지 생성</Button>
+      </Box>
+      <Box sx={{ marginLeft: "750px", marginTop: "-710px" }}>
+        <WatchToggle></WatchToggle>
+      </Box>
     </Box>
   );
 }
