792ac9cb4fa7c4c422a7617f0b0da7b756eb1d26
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-11-01 00:10:45 +0900

    [Add] DatePicker 추가

--- frontend/kkobak/src/component/module/DatePicker.js
+++ frontend/kkobak/src/component/module/DatePicker.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import DatePicker from 'react-date-picker';
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+
+import "react-date-picker/dist/DatePicker.css"
+import "react-calendar/dist/Calendar.css"
+
+export default function DatePick() {
+  const [value, onChange] = useState(new Date());
+  return (
+    <Box>
+      <DatePicker onChange={onChange} value={value} minDate={new Date()}/>
+    </Box>
+  );
+}
