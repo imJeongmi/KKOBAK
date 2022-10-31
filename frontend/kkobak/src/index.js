1a01d8fde8bc9570a10a3c99274183b6146cd89b
cheecrma <cheecrma@gmail.com>
2022-11-01 08:46:54 +0900

    [Update] 네브바 뒤에 공간 생성

--- frontend/kkobak/src/index.js
+++ frontend/kkobak/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-
+import "./index.css";
 import Theme from "./configs/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
