634ce05deca6ff0f1b4a0a1a1b451f9a1e0c167a
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-11-01 16:34:17 +0900

    [Add] first page

--- wearos/app/src/main/java/com/example/kkobak/MainActivity.java
+++ wearos/app/src/main/java/com/example/kkobak/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.kkobak;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.kkobak.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
     private ActivityMainBinding binding;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("btn click");
+            }
+        });
     }
 }
