8f26ec5edf70342ce80e8c4265384480eb707748
leemyo <leemyo_@naver.com>
2022-11-02 06:22:00 +0900

    [Add] login input page

--- wearos/app/src/main/java/com/example/kkobak/LoginActivity.java
+++ wearos/app/src/main/java/com/example/kkobak/LoginActivity.java
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+
+    private EditText et_email;
+    private EditText et_pw;
+    private Button btn_loginchk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        System.out.println("login page");
+
+        et_email = findViewById(R.id.et_email);
+        et_pw = findViewById(R.id.et_pw);
+        btn_loginchk = findViewById(R.id.btn_loginchk);
+
+        btn_loginchk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println(et_email.getText());
+                System.out.println(et_pw.getText());
+            }
+        });
     }
 }
