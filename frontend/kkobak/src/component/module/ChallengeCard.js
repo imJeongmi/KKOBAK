1bed7fbfc3bae5e13109c6190a96d452cb6e8122
Jeongmi Park <qkrwjdal419@gmail.com>
2022-11-02 13:11:30 +0900

    [Update] ChallengeCard layout

--- frontend/kkobak/src/component/module/ChallengeCard.js
+++ frontend/kkobak/src/component/module/ChallengeCard.js
@@ -1,72 +1,74 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Text from "../atom/Text";
-import Watch from "../../static/watch.svg";
-import TagLabel from "../atom/TagLabel";
+import { styled } from "@mui/system";
+
+import Text from "component/atom/Text";
+import TagLabel from "component/atom/TagLabel";
+import Watch from "static/watch.svg";
+
+const CardBox = styled(Box)(
+  () => `
+  width: 260px;
+  height: 260px;
+  margin: 20px 10px;
+  background-color: white;
+  border-radius: 8px;
+  `
+);
+
+const ImageBox = styled(Box)(
+  () => `
+  width: 240px;
+  height: 140px;
+  margin: 10px;
+  border-radius: 8px;
+  overflow: hidden;
+  `
+);
+
+const FilterBox = styled(Box)(
+  () => `
+  // width: 170px;
+  height: 30px;
+  margin: 5px 15px;
+  display: flex;
+  justify-content: space-between;
+  `
+);
+
+const TextBox = styled(Box)(
+  () => `
+  margin: 5px 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  `
+);
 
 export default function ChallengeCard(props) {
   return (
-      <Box
-        sx={{
-          width: "260px",
-          height: "260px",
-          margin: "20px 10px",
-          backgroundColor: "white",
-          borderRadius: 2,
-          boxShadow: 5,
-        }}
-      >
-        <Box sx={{ height: "10px" }}></Box>
-        <Box
-          sx={{
-            marginX: "10px",
-            width: "240px",
-            height: "140px",
-            backgroundColor: "gray",
-            borderRadius: 2,
-            overflow: "hidden",
-          }}
-        >
-          {/* <img src={props.src} width="100%" /> */}
-          <img
-            src="https://t1.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/2xMI/image/eRlaPLwEH3RjexrX_uVmse2g-LU.jpg"
-            width="100%"
-          />
-        </Box>
-        <Box>
-          <Box
-            sx={{
-              marginLeft: "10px",
-              width: "170px",
-              height: "50px",
-              backgroundColor: "white",
-              marginTop: "5px",
-              float: "left",
-            }}
-          >
-            {/* 태그들 앞에 3개만 보이도록 수정 map 활용 태그 모양 수정 필요*/}
-            <TagLabel>{props.tag}</TagLabel>
-          </Box>
-          <Box
-            sx={{
-              marginRight: "10px",
-              marginTop: "10px",
-              width: "40px",
-              height: "40px",
-              float: "right",
-            }}
-          >
-            {/* 워치 이용 판단 */}
-            <img src={Watch} width={34} height={34} />
-          </Box>
-        </Box>
-        <Box sx={{ marginLeft: "10px" }}>
-          <Box height="55px" />
-          <Text>{props.title}</Text>
-          <Text size="m">매일 아침 러닝!</Text>
-          <Text my="2px">{props.period}</Text>
-          <Text my="5px" size="s">기간</Text>
-        </Box>
-      </Box>
+    <CardBox>
+      <ImageBox>
+        {/* <img src={props.src} width="100%" height="100%" /> */}
+        <img
+          src="https://t1.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/2xMI/image/eRlaPLwEH3RjexrX_uVmse2g-LU.jpg"
+          width="100%"
+          height="100%"
+        />
+      </ImageBox>
+      <FilterBox>
+        {/* 태그들 앞에 3개만 보이도록 수정 map 활용 태그 모양 수정 필요*/}
+        <TagLabel>{props.tag}</TagLabel>
+        <img src={Watch} width="25px" />
+      </FilterBox>
+      <TextBox>
+        {/* <Text>{props.title}</Text> */}
+        <Text size="m" weight="medium">매일 아침 러닝!</Text>
+        {/* <Text my="2px">{props.period}</Text> */}
+        <Text my="5px" size="12px" color="grey">
+          기간
+        </Text>
+      </TextBox>
+    </CardBox>
   );
 }
