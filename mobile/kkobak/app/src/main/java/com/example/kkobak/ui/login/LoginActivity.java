036100ccd898c0f6fec6f3c61abe0241a93368b8
HumanHyeon <sehyeon1997@gmail.com>
2022-11-03 03:16:13 +0900

    [Update] code refectoring

--- mobile/kkobak/app/src/main/java/com/example/kkobak/ui/login/LoginActivity.java
+++ mobile/kkobak/app/src/main/java/com/example/kkobak/ui/login/LoginActivity.java
@@ -103,7 +103,6 @@ public class LoginActivity extends AppCompatActivity {
 
     public void moveRegisterPage(View v) {
         Intent intent = new Intent(this, RegisterActivity.class);
-//        startActivity(intent);
         startActivityForResult(intent, REGISTER_PAGE);
     }
 
