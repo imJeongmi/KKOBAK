f27b83b52aad0b32345ed03557ab9029db0e7b08
Jeongmi Park <qkrwjdal419@gmail.com>
2022-11-02 16:23:45 +0900

    [Update] ChallengeCardList layout

--- frontend/kkobak/src/component/module/ChallengeCardList.js
+++ frontend/kkobak/src/component/module/ChallengeCardList.js
@@ -1,57 +1,61 @@
 import React, { useState } from "react";
-import ChallengeCard from "./ChallengeCard";
 import Box from "@mui/material/Box";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+
 import Button from "component/atom/TextButton";
+import MainBox from "component/atom/MainBox";
 import WatchToggle from "component/atom/WatchToggle";
+import ChallengeCard from "component/module/ChallengeCard";
 
 export default function ChallengeCardList() {
   const [ChallengeList, setChallengeList] = useState([]);
 
   return (
-    <Box>
+    <MainBox flexDir="col">
       <Box
         sx={{
-          width: "924px",
-          minHeight: "650px",
-          backgroundColor: "#F5F5F5",
-          verticalAlign: "middle",
+          width: "90%",
+          margin: "20px 20px 0 20px",
           display: "flex",
-          flexWrap: "wrap",
-          borderRadius: 5,
+          justifyContent: "start",
         }}
       >
-        {/* 받아온 챌린지리스트 순회하기 */}
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            marginTop: "70px",
-            width: "924px",
-            height: "600px",
-          }}
-        >
-          <ChallengeCard />
-          <ChallengeCard />
-          <ChallengeCard />
-          <ChallengeCard />
-          <ChallengeCard />
-          <ChallengeCard />
-        </Box>
-
-        <Box sx={{ marginLeft: "275px", marginTop: "45px" }}>
-          <Stack spacing={2}>
-            <Pagination count={10} shape="rounded" />
-          </Stack>
-        </Box>
-        <Box sx={{ marginLeft: "735px", marginTop: "-80px" }}>
-          <Button size="ss">챌린지 생성</Button>
-        </Box>
-        <Box sx={{ marginLeft: "750px", marginTop: "-720px" }}>
-          <WatchToggle></WatchToggle>
-        </Box>
+        <WatchToggle />
       </Box>
-    </Box>
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "70vh",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <ChallengeCard />
+        <ChallengeCard />
+        <ChallengeCard />
+        <ChallengeCard />
+        <ChallengeCard />
+        <ChallengeCard />
+      </Box>
+      <Box
+        sx={{
+          width: "95%",
+          height: "40px",
+          my: "5px",
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <Button size="ss" my="0">챌린지 생성</Button>
+      </Box>
+
+      <Box>
+        <Stack spacing={2}>
+          <Pagination count={10} shape="rounded" />
+        </Stack>
+      </Box>
+    </MainBox>
   );
 }
