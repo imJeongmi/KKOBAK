71c2340448e7dd54fa39448ea2ad9b87cca220d9
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-11-02 17:09:50 +0900

    [Edit] 스타일 정리

--- frontend/kkobak/src/component/atom/ImageButton.js
+++ frontend/kkobak/src/component/atom/ImageButton.js
@@ -11,7 +11,7 @@ const ImageButtonBase = styled(Button)(
   left: 0;
   width: 100%;
   height: 100%;
- padding-top: 100%;
+  padding-top: 100%;
   background: linear-gradient(135deg, #FFFFFF 0%, #F8F8F8 29.48%, #F3F3F3 52.92%, #EFEFEF 71.15%, #ECECEC 86.77%, #E9E9E9 100%);
   box-shadow: -5px 5px 10px rgba(207, 207, 207, 0.2), 5px -5px 10px rgba(207, 207, 207, 0.2), -5px -5px 10px rgba(255, 255, 255, 0.9), 5px 5px 13px rgba(207, 207, 207, 0.9), inset 1px 1px 2px rgba(255, 255, 255, 0.3), inset -1px -1px 2px rgba(207, 207, 207, 0.5);
   border-radius: 20px;
