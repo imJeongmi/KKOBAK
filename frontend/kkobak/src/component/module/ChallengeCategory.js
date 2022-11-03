781bab845259d13c762fd685afa7a3f2b327b00a
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-11-03 10:58:57 +0900

    [Edit] 챌린지 카테고리 스타일

--- frontend/kkobak/src/component/module/ChallengeCategory.js
+++ frontend/kkobak/src/component/module/ChallengeCategory.js
@@ -1,25 +1,53 @@
 import { Box } from '@mui/system';
 import { getCategoryList } from 'api/Category';
+import ImageButton from 'component/atom/ImageButton';
+import MainBox from 'component/atom/MainBox';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-export default function ChallengeCategory() {
-  const [category, setCategory] = useState();
+const BoxStyle = {
+  height: "100vh",
+  width: "70vw",
+  display: "flex",
+  // alignItems: "center",
+  flexDirection: "column",
+};
+
+const CardStyle = {
+  width: "95%",
+  margin: "auto",
+  minHeight: "80vh",
+  backgroundColor: "#F5F5F5",
+  textAlign: "center",
+  display: "flex",
+  justifyContent: "space-around",
+  borderRadius: "20px",
+};
+
+
+export default function ChallengeCategory({ setCategory }) {
+  const [categoryList, setCategoryList] = useState([]);
+
   useEffect(() => {
     getCategoryList(getCategoryListSuccess, getCategoryListfail)
-    }, [])
-  
+  }, [])
+
   function getCategoryListSuccess(res) {
-    setCategory(res.data)
+    setCategoryList(res.data)
     console.log(res.data)
   }
 
   function getCategoryListfail(err) {
     console.log(err)
-  } 
+  }
 
-return (
-  <Box></Box>
-)
+  return (
+    <Box sx={BoxStyle}>
+      <Box sx={CardStyle}>
+        <ImageButton src={categoryList[0]?.imgurl} size="l" id={1} onClick={(e) => setCategory(e.target.id)}>운동</ImageButton>
+        <ImageButton src={categoryList[1]?.imgurl} size="l" id={2} onClick={(e) => setCategory(e.target.id)}>생활습관</ImageButton>
+      </Box>
+    </Box >
+  )
 }
