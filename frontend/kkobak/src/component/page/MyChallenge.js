1ca118d4621d27ae8bc64c04d83d229e35d348ea
cheecrma <cheecrma@gmail.com>
2022-11-03 12:28:52 +0900

    [Update] 나의 챌린지 리스트 작업

--- frontend/kkobak/src/component/page/MyChallenge.js
+++ frontend/kkobak/src/component/page/MyChallenge.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Text from "component/atom/Text";
 import Box from "@mui/material/Box";
-import ChallengeCardList from "component/module/ChallengeCardList";
+import MyChallengeCardList from "component/module/MyChallengeCardList";
 import SideBar from "component/atom/SideBar";
 import CategoryToggle from "component/atom/CategoryToggle";
 import MyChallengeCarousel from "component/module/MyChallengeCarousel";
@@ -26,13 +26,13 @@ export default function Challenge() {
         <Text size="l" weight="bold" mt="30" my="15">
           {user.nickName}님의 챌린지리스트
         </Text>
-        <ChallengeCardList />
+        <MyChallengeCardList />
       </Box>
       <SideBar>
         {/* paddingY: "30px" 임시로 아래 패딩Y 뺐음. 화면 확인 위해 */}
         <Box sx={{ marginLeft: "10px" }}>
           <Text size="l" weight="bold">
-            승리님의 챌린지 현황
+            {user.nickName}님의 챌린지 현황
           </Text>
         </Box>
         <Box sx={{ float: "right", marginRight: "40px" }}>
