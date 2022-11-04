658a1550224c66756795b2c44d70831edb1a6823
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-11-04 14:34:45 +0900

    [Update] check gps

--- wearos/app/src/main/java/com/example/kkobak/ListActivity.java
+++ wearos/app/src/main/java/com/example/kkobak/ListActivity.java
@@ -71,9 +71,10 @@ public class ListActivity extends Activity {
         todoAdapter.setOnLongItemClickListener(new TodoAdapter.OnLongItemClickListener() {
             @Override
             public void onLongItemClick(int pos) {
+                System.out.println("꾹 눌렀습니다");
                 Toast.makeText(getApplicationContext(), "임시적으로 GPS로 연결", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(ListActivity.this, GpsActivity.class);
-                startActivity(intent);
+//                startActivity(intent);
             }
         });
 
