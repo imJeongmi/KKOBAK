ec3042169a4bc06ac599a34d53551e6f7d01df45
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-11-07 14:50:10 +0900

    [Edit] 지도 그림 그리기 완료

--- frontend/kkobak/src/component/atom/ChallengeMap.js
+++ frontend/kkobak/src/component/atom/ChallengeMap.js
@@ -1,11 +1,24 @@
 import React from "react";
-import {Map} from "react-kakao-maps-sdk";
+import { Map, Polyline } from "react-kakao-maps-sdk";
 
 export default function ChallengeMap() {
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "360px" }}
+      center={{ lat: 33.452344169439975, lng: 126.56878163224233 }}
+      style={{ display: "inline-block", width: "50%", height: "360px" }}
     >
+      <Polyline
+        path={[
+          [
+            { lat: 33.452344169439975, lng: 126.56878163224233 },
+            { lat: 33.452739313807456, lng: 126.5709308145358 },
+            { lat: 33.45178067090639, lng: 126.572688693875 },
+          ],
+        ]}
+        strokeWeight={5} // 선의 두께 입니다
+        strokeColor={"#000000"} // 선의 색깔입니다
+        strokeOpacity={1} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle={"solid"} // 선의 스타일입니다
+      />
     </Map>)
 }
