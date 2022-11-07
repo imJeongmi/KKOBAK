e041a6aa89249d045cc0d7e791acea6e35b01c5f
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-11-07 17:50:16 +0900

    [Add] bpm 정보 불러오기 api 생성

--- backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/controller/StatbpmController.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/controller/StatbpmController.java
@@ -1,18 +1,13 @@
 package com.a104.freeproject.Statbpm.controller;
 
-import com.a104.freeproject.Member.request.JoinRequest;
-import com.a104.freeproject.Member.response.TokenResponse;
 import com.a104.freeproject.Statbpm.request.BpmInputRequest;
-import com.a104.freeproject.Statbpm.service.StatbpmService;
+import com.a104.freeproject.Statbpm.response.BpmResultResponse;
 import com.a104.freeproject.Statbpm.service.StatbpmServiceImpl;
 import com.a104.freeproject.advice.exceptions.NotFoundException;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,6 +23,13 @@ public class StatbpmController {
     @ApiOperation(value="[확인] 데이터 입력", notes = "")
     public ResponseEntity<Boolean> addData(@RequestBody BpmInputRequest input, HttpServletRequest req) throws NotFoundException {
         return ResponseEntity.ok().body(statbpmService.addData(input, req));
+    }
+
+    @GetMapping("/list/{year}/{month}/{day}/{cid}")
+    @ApiOperation(value="데이터 시도 목록 반환", notes = "'/api/bpm/list/2022/11/07/1' 형식으로 사용")
+    public ResponseEntity<BpmResultResponse> addData(@PathVariable("year") String year, @PathVariable("month") String month,
+                                                     @PathVariable("day") String day, @PathVariable("cid") Long cid, HttpServletRequest req) throws NotFoundException {
+        return ResponseEntity.ok().body(statbpmService.getTryList(year, month, day, cid, req));
     }
 
 }
--- backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/service/StatbpmServiceImpl.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/service/StatbpmServiceImpl.java
@@ -3,6 +3,8 @@ package com.a104.freeproject.Statbpm.service;
 import com.a104.freeproject.Challenge.entity.Challenge;
 import com.a104.freeproject.Challenge.repository.ChallengeRepository;
 import com.a104.freeproject.Challenge.service.ChallengeServiceImpl;
+import com.a104.freeproject.Log.entity.Log;
+import com.a104.freeproject.Log.repository.LogRepository;
 import com.a104.freeproject.Member.entity.Member;
 import com.a104.freeproject.Member.service.MemberServiceImpl;
 import com.a104.freeproject.PrtChl.entity.PrtChl;
@@ -10,13 +12,22 @@ import com.a104.freeproject.PrtChl.repository.PrtChlRepository;
 import com.a104.freeproject.Statbpm.entity.Statbpm;
 import com.a104.freeproject.Statbpm.repository.StatbpmRepository;
 import com.a104.freeproject.Statbpm.request.BpmInputRequest;
+import com.a104.freeproject.Statbpm.response.BpmListResponse;
+import com.a104.freeproject.Statbpm.response.BpmResultResponse;
+import com.a104.freeproject.Statgps.entity.Statgps;
+import com.a104.freeproject.Statgps.response.GpsResultResponse;
+import com.a104.freeproject.Statgps.response.ResultResponse;
 import com.a104.freeproject.advice.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -27,6 +38,7 @@ public class StatbpmServiceImpl implements StatbpmService{
     private final ChallengeRepository challengeRepository;
     private final PrtChlRepository prtChlRepository;
     private final StatbpmRepository statbpmRepository;
+    private final LogRepository logRepository;
 
     @Override
     public boolean addData(BpmInputRequest input, HttpServletRequest req) throws NotFoundException {
@@ -50,5 +62,69 @@ public class StatbpmServiceImpl implements StatbpmService{
         statbpmRepository.save(statbpm);
 
         return true;
+    }
+
+    @Override
+    public BpmResultResponse getTryList(String year, String month, String day, Long cid, HttpServletRequest req) throws NotFoundException {
+        Member member = memberService.findEmailbyToken(req);
+
+        if(!challengeRepository.existsById(cid))
+            throw new NotFoundException("해당 챌린지가 존재하지 않습니다.");
+        Challenge c = challengeRepository.findById(cid).get();
+
+        if(!prtChlRepository.existsByChallengeAndMember(c,member))
+            throw new NotFoundException("참여하지 않은 챌린지입니다.");
+        PrtChl p = prtChlRepository.findByChallengeAndMember(c,member);
+
+        LocalDate date = LocalDate.of(Integer.parseInt(year),Integer.parseInt(month),Integer.parseInt(day));
+
+        if(!logRepository.existsByPrtChlAndDate(p,date))
+            throw new NotFoundException("오늘 해당 챌린지의 로그가 존재하지 않습니다.");
+        Log log = logRepository.findByPrtChlAndDate(p,date);
+
+        List<Statbpm> statbpmList = statbpmRepository.findByChkAndPrtChl(date.toString(),p);
+        if(statbpmList.size()==0)
+            return BpmResultResponse.builder()
+                    .flag(false)
+                    .bpmList(new LinkedList<BpmListResponse>())
+                    .maxBpm(0).minBpm(0).avgBpm(0)
+                    .build();
+
+        boolean flag = statbpmList.get(statbpmList.size()-1).isSuccess();
+        LocalDateTime sendTime = statbpmList.get(statbpmList.size()-1).getChk();
+        if(!flag){
+            for(int i = statbpmList.size()-2;i>=0;i--){
+                if(flag) {
+                    flag = statbpmList.get(statbpmList.size()-1).isSuccess();
+                    sendTime = statbpmList.get(statbpmList.size()-1).getChk();
+                    break;
+                }
+            }
+        }
+
+        List<Statbpm> list = statbpmRepository.findByChkTimeAndPrtChl(p,sendTime);
+        List<BpmListResponse> output = new LinkedList<>();
+        int maxbpm=list.get(0).getBpm();
+        int minbpm=list.get(0).getBpm();
+        int sumbpm=0;
+        for(Statbpm statbpm:list){
+            sumbpm+=statbpm.getBpm();
+            maxbpm=Math.max(maxbpm,statbpm.getBpm());
+            minbpm=Math.min(minbpm,statbpm.getBpm());
+
+            output.add(BpmListResponse.builder()
+                    .bpm(statbpm.getBpm()).time(statbpm.getTime())
+                    .build());
+        }
+
+        int avgbpm=sumbpm/list.size();
+
+        return BpmResultResponse.builder()
+                .flag(flag)
+                .bpmList(output)
+                .maxBpm(maxbpm)
+                .minBpm(minbpm)
+                .avgBpm(avgbpm)
+                .build();
     }
 }
