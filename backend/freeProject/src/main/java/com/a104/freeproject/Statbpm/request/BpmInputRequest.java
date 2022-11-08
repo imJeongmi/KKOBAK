22796109189129f15f7e1154060e4bee717b5d22
홍성영 <hong_1598@naver.com>
2022-11-08 12:22:50 +0900

    [ETC] 심장 테스트용 주석 추가

--- backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/request/BpmInputRequest.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/Statbpm/request/BpmInputRequest.java
@@ -14,13 +14,13 @@ public class BpmInputRequest {
     private Long chlId;
 
     @Setter
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime time;
 
     private int bpm;
 
     @Setter
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime chk;
 
 }
