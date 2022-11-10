afb74a0dca3f3d3feb6b004d3ad9d1ccbc475321
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-11-10 17:33:47 +0900

    [Update] watch 관련 api

--- backend/freeProject/src/main/java/com/a104/freeproject/PrtChl/repository/PrtChlRepository.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/PrtChl/repository/PrtChlRepository.java
@@ -23,10 +23,10 @@ public interface PrtChlRepository extends JpaRepository<PrtChl,Long> {
     @Query(value="select * from prt_chl where is_fin=false and `member_id`=:m", nativeQuery = true)
     List<PrtChl> findByMember(@Param("m") Member m);
 
-    @Query(value="select * from prt_chl as prtchl where is_fin=false and member_id=:m and (select watch from challenge where id = prtchl.id)=:useWatch", nativeQuery = true)
+    @Query(value="select * from prt_chl as prtchl where is_fin=false and member_id=:m and (select watch from challenge where id = prtchl.challenge_id)=:useWatch", nativeQuery = true)
     List<PrtChl> findAllByWatchAndMember(boolean useWatch, Pageable pageable, Member m);
 
-    @Query(value="select * from prt_chl as prtchl where is_fin=false and member_id=:m and (select watch from challenge where id = prtchl.id)=:useWatch", nativeQuery = true)
+    @Query(value="select * from prt_chl as prtchl where is_fin=false and member_id=:m and (select watch from challenge where id = prtchl.challenge_id)=:useWatch", nativeQuery = true)
     Page<PrtChl> findAllByWatchAndMember(Pageable pageable, boolean useWatch, Member m);
 
 }
