ca39dfe54840946b3b86469601dce1563b25dd90
HyoJung <52395465+leemyo@users.noreply.github.com>
2022-11-10 10:21:39 +0900

    [Update] base url 수정

--- backend/freeProject/src/main/java/com/a104/freeproject/config/SwaggerConfig.java
+++ backend/freeProject/src/main/java/com/a104/freeproject/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-//                .host("kkobak.ml")
+                .host("kkobak.ml")
                 .useDefaultResponseMessages(false)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
