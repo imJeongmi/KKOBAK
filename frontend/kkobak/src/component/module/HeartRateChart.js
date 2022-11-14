ea1032bb544b863f83c8164e6ab48c2a26491f85
cheecrma <cheecrma@gmail.com>
2022-11-14 17:18:48 +0900

    [Update] 내 챌린지 상세 통계 부분

--- frontend/kkobak/src/component/module/HeartRateChart.js
+++ frontend/kkobak/src/component/module/HeartRateChart.js
@@ -49,28 +49,37 @@ export default function LineChartPage({ findTime }) {
       <Text weight="semibold" size="15px" my="3">
         {"심박수 정보"}
       </Text>
-
-      <Box sx={{ fontFamily: "SUIT-Medium", fontSize: "12px" }}>
-        <LineChart width={400} height={150} data={bpm}>
-          <XAxis />
-          {/* 데이터 타임 오는 시간 오후 4시가 16시가 아니라 04시로 옴. 
+      <Box sx={{ flexDirecton: "row" }}>
+        <Box
+          sx={{
+            fontFamily: "SUIT-Medium",
+            fontSize: "12px",
+            flex: 1,
+            float: "left",
+          }}
+        >
+          <LineChart width={300} height={150} data={bpm}>
+            <XAxis />
+            {/* 데이터 타임 오는 시간 오후 4시가 16시가 아니라 04시로 옴. 
           time이 현재 2022-11-08 04:28:43으로 오는데 x축에 표시하기 위해 16:28:43 이렇게 객체 안에 하나 더 추가해줘도 좋음. checktime이런거 하나 추가해서*/}
-          {/* <XAxis dataKey="time" /> */}
-          <YAxis domain={[60, 160]} />
-          {/* <Tooltip  /> */}
-          {/* <Legend /> */}
-          <Line
-            type="monotone"
-            dataKey="bpm"
-            stroke="black"
-            // activeDot={{ r: 1 }}
-          />
-        </LineChart>
+            {/* <XAxis dataKey="time" /> */}
+            <YAxis domain={[60, 160]} />
+            {/* <Tooltip  /> */}
+            {/* <Legend /> */}
+            <Line
+              type="monotone"
+              dataKey="bpm"
+              stroke="black"
+              // activeDot={{ r: 1 }}
+            />
+          </LineChart>
+        </Box>
+        <Box sx={{ flex: 1 }}>
+          <Text size="13px">{`평균 심박수 : ${stat.avgBpm}`}</Text>
+          <Text size="13px" mt="3">{`최고 심박수 : ${stat.maxBpm}`}</Text>
+          <Text size="13px" mt="3">{`최저 심박수 : ${stat.minBpm}`}</Text>
+        </Box>
       </Box>
-
-      <Text size="13px">{`평균 심박수 : ${stat.avgBpm}`}</Text>
-      <Text size="13px" mt="3">{`최고 심박수 : ${stat.maxBpm}`}</Text>
-      <Text size="13px" mt="3">{`최저 심박수 : ${stat.minBpm}`}</Text>
     </Box>
   );
 }
