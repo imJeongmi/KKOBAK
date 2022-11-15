5ef8a3c745988c64c4203b1936a0b6b18c25f232
Gyeongwon <97663864+wynne-baek@users.noreply.github.com>
2022-11-15 13:13:34 +0900

    새로고침 추가

--- frontend/kkobak/src/component/module/EditProfileModal.js
+++ frontend/kkobak/src/component/module/EditProfileModal.js
@@ -69,6 +69,7 @@ export default function EditProfileModal({ open, setOpen, nickName, imgurl }) {
   }
 
   function changeProfileImageSuccess(res) {
+    window.location.reload();
   }
 
   function changeProfileImageFail(err) {
