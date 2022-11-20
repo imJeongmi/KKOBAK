1470563e9620f22230524914b5ced2a4adb36fbb
leemyo <leemyo_@naver.com>
2022-11-20 22:03:26 +0900

    [Update] main page design

--- wearos/app/src/main/java/com/example/kkobak/MainActivity.java
+++ wearos/app/src/main/java/com/example/kkobak/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,7 +20,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 public class MainActivity extends Activity {
 
     private ActivityMainBinding binding;
-    private Button btn_login;
+    private ImageButton btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
